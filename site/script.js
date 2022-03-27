cb7e7be0f4e177dcd661767b694732c456be327f
Bhavnoor-Coders-1010 <singh.bhavnoor.4854@gmail.com>
2022-03-27 17:59:00 +0530

    My first websiste

---
+++ site/script.js
@@ -0,0 +1,12 @@
+var x = "Hello World!!"
+
+
+// Defining functions
+// function a () {
+// 	console.log("Function a --> type 1")
+// }
+
+// var a = function () {console.log("Function a --> type 2")}
+
+//calling/invocation a function
+// a();
